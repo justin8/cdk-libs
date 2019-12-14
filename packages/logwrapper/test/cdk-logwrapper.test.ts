ac57eaced0e2c0a0960fcfecd1bbe79b3cf46cce
Justin Dray <justin@dray.be>
2019-12-14 23:12:37 +1000

    Add tests to logwrapper

--- packages/logwrapper/test/cdk-logwrapper.test.ts
+++ packages/logwrapper/test/cdk-logwrapper.test.ts
@@ -1,21 +1,56 @@
-import { expect as expectCDK, haveResource, SynthUtils } from '@aws-cdk/assert';
-import cdk = require('@aws-cdk/core');
-import CdkLogwrapper = require('../lib/index');
+import {
+  expect as expectCDK,
+  haveResource,
+  expect,
+  SynthUtils,
+  HaveResourceAssertion
+} from "@aws-cdk/assert";
+import cdk = require("@aws-cdk/core");
+import { LogGroupWrapper } from "../lib/index";
+import { FilterPattern } from "@aws-cdk/aws-logs";
 
-test('SQS Queue Created', () => {
-    const app = new cdk.App();
-    const stack = new cdk.Stack(app, "TestStack");
-    // WHEN
-    new CdkLogwrapper.CdkLogwrapper(stack, 'MyTestConstruct');
-    // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue"));
+const props = {
+  logGroupName: "/var/log/foo.log",
+  filterPattern: FilterPattern.anyTerm("error", "Error"),
+  noLogsAlarm: {
+    enabled: true,
+    evaluationPeriods: 1,
+    metricPeriod: cdk.Duration.days(1),
+    threshold: 5
+  },
+  errorsAlarm: {
+    enabled: true,
+    evaluationPeriods: 1,
+    metricPeriod: cdk.Duration.minutes(5),
+    threshold: 1
+  }
+};
+
+const basicApp = new cdk.App();
+const basicStack = new cdk.Stack(basicApp, "BasicStack");
+new LogGroupWrapper(basicStack, "MyTestConstruct", props);
+
+test("Log group created", () => {
+  expectCDK(basicStack).to(haveResource("AWS::Logs::LogGroup"));
 });
 
-test('SNS Topic Created', () => {
-  const app = new cdk.App();
-  const stack = new cdk.Stack(app, "TestStack");
-  // WHEN
-  new CdkLogwrapper.CdkLogwrapper(stack, 'MyTestConstruct');
-  // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+test("Metric Filter group created", () => {
+  expectCDK(basicStack).to(haveResource("AWS::Logs::MetricFilter"));
+});
+
+test("Errors Alarm created", () => {
+  expectCDK(basicStack).to(
+    haveResource("AWS::CloudWatch::Alarm", {
+      MetricName: "MyTestConstructErrors"
+    })
+  );
+});
+
+test("No Logs Alarm created", () => {
+  expectCDK(basicStack).to(
+    haveResource("AWS::CloudWatch::Alarm", {
+      MetricName: "IncomingLogEvents",
+      Namespace: "AWS/Logs"
+    })
+  );
 });
