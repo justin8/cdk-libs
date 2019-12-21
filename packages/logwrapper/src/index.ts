ab3f5fdb33e9d36f508167567ea7a03396e03f08
Justin Dray <justin@dray.be>
2019-12-21 21:33:19 +1000

    Make additional alarm properties optional if the alarm is disabled. Optionally subscribe to an sns topic

--- packages/logwrapper/src/index.ts
+++ packages/logwrapper/src/index.ts
@@ -1,5 +1,7 @@
 import { IFilterPattern, LogGroup, MetricFilter } from "@aws-cdk/aws-logs";
 import { Construct, Duration } from "@aws-cdk/core";
+import { SnsAction } from "@aws-cdk/aws-cloudwatch-actions";
+import { ITopic } from "@aws-cdk/aws-sns";
 import {
   Alarm,
   ComparisonOperator,
@@ -12,17 +14,18 @@ export interface LogGroupProps {
   filterPattern: IFilterPattern;
   noLogsAlarm: AlarmProps;
   errorsAlarm: AlarmProps;
+  alarmsTopic?: ITopic;
 }
 
 export interface AlarmProps {
   enabled: boolean;
-  evaluationPeriods: number;
-  metricPeriod: Duration;
-  threshold: number;
+  evaluationPeriods?: number;
+  metricPeriod?: Duration;
+  threshold?: number;
 }
 
 export class LogGroupWrapper extends Construct {
-  public alarms: Array<Alarm>;
+  public alarms: Array<Alarm> = [];
 
   constructor(parent: Construct, name: string, props: LogGroupProps) {
     super(parent, name);
@@ -45,21 +48,20 @@ export class LogGroupWrapper extends Construct {
     });
 
     // Alarms
-    this.alarms = [];
     if (errorsAlarm.enabled) {
       this.alarms.push(
         new Alarm(this, `${name}ErrorsAlarm`, {
           actionsEnabled: true,
           alarmDescription: `An error was found in the ${props.logGroupName} log group`,
-          threshold: errorsAlarm.threshold,
+          threshold: errorsAlarm.threshold!,
           comparisonOperator:
             ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
-          evaluationPeriods: errorsAlarm.evaluationPeriods,
+          evaluationPeriods: errorsAlarm.evaluationPeriods!,
           treatMissingData: TreatMissingData.NOT_BREACHING,
           metric: new Metric({
             metricName: errorMetricName,
             namespace: metricNamespace,
-            period: errorsAlarm.metricPeriod
+            period: errorsAlarm.metricPeriod!
           })
         })
       );
@@ -70,19 +72,26 @@ export class LogGroupWrapper extends Construct {
         new Alarm(this, `${name}NoLogsAlarm`, {
           actionsEnabled: true,
           alarmDescription: `No logs have been found recently for the ${props.logGroupName} log group`,
-          threshold: noLogsAlarm.threshold,
+          threshold: noLogsAlarm.threshold!,
           comparisonOperator: ComparisonOperator.LESS_THAN_THRESHOLD,
-          evaluationPeriods: noLogsAlarm.evaluationPeriods,
+          evaluationPeriods: noLogsAlarm.evaluationPeriods!,
           treatMissingData: TreatMissingData.BREACHING,
           statistic: "sum",
           metric: new Metric({
             metricName: "IncomingLogEvents",
             namespace: "AWS/Logs",
-            period: noLogsAlarm.metricPeriod,
+            period: noLogsAlarm.metricPeriod!,
             dimensions: { LogGroupName: logGroup.logGroupName }
           })
         })
       );
+    }
+
+    if (props.alarmsTopic) {
+      for (let i in this.alarms) {
+        let action = new SnsAction(props.alarmsTopic);
+        this.alarms[i].addAlarmAction(action);
+      }
     }
   }
 }
--- packages/logwrapper/test/cdk-logwrapper.test.ts
+++ packages/logwrapper/test/cdk-logwrapper.test.ts
@@ -8,8 +8,9 @@ import {
 import cdk = require("@aws-cdk/core");
 import { LogGroupWrapper } from "../lib/index";
 import { FilterPattern } from "@aws-cdk/aws-logs";
+import { Topic } from "@aws-cdk/aws-sns";
 
-const props = {
+let defaultProps: any = {
   logGroupName: "/var/log/foo.log",
   filterPattern: FilterPattern.anyTerm("error", "Error"),
   noLogsAlarm: {
@@ -28,7 +29,9 @@ const props = {
 
 const basicApp = new cdk.App();
 const basicStack = new cdk.Stack(basicApp, "BasicStack");
-new LogGroupWrapper(basicStack, "MyTestConstruct", props);
+const alarmsTopic = new Topic(basicStack, "topic");
+defaultProps.alarmsTopic = alarmsTopic;
+new LogGroupWrapper(basicStack, "MyTestConstruct", defaultProps);
 
 test("Log group created", () => {
   expectCDK(basicStack).to(haveResource("AWS::Logs::LogGroup"));
@@ -51,6 +54,64 @@ test("No Logs Alarm created", () => {
     haveResource("AWS::CloudWatch::Alarm", {
       MetricName: "IncomingLogEvents",
       Namespace: "AWS/Logs"
+    })
+  );
+});
+
+test("No logs alarm can be disabled", () => {
+  let localProps = { ...defaultProps };
+  localProps.noLogsAlarm = { enabled: false };
+  localProps.alarmsTopic = null;
+  const basicApp = new cdk.App();
+  const basicStack = new cdk.Stack(basicApp, "BasicStack");
+  new LogGroupWrapper(basicStack, "MyTestConstruct", localProps);
+  expectCDK(basicStack).notTo(
+    haveResource("AWS::CloudWatch::Alarm", {
+      MetricName: "IncomingLogEvents",
+      Namespace: "AWS/Logs"
+    })
+  );
+});
+
+test("Errors alarm can be disabled", () => {
+  let localProps = { ...defaultProps };
+  localProps.errorsAlarm = { enabled: false };
+  localProps.alarmsTopic = null;
+  const basicApp = new cdk.App();
+  const basicStack = new cdk.Stack(basicApp, "BasicStack");
+  new LogGroupWrapper(basicStack, "MyTestConstruct", localProps);
+  expectCDK(basicStack).notTo(
+    haveResource("AWS::CloudWatch::Alarm", {
+      MetricName: "MyTestConstructErrors"
+    })
+  );
+});
+
+test("Alarms are subscribed to given topic", () => {
+  expectCDK(basicStack).to(
+    haveResource("AWS::CloudWatch::Alarm", {
+      MetricName: "IncomingLogEvents",
+      Namespace: "AWS/Logs",
+      AlarmActions: [{ Ref: "topic69831491" }]
+    })
+  );
+  expectCDK(basicStack).to(
+    haveResource("AWS::CloudWatch::Alarm", {
+      MetricName: "MyTestConstructErrors",
+      AlarmActions: [{ Ref: "topic69831491" }]
+    })
+  );
+});
+
+test("No subscriptions when alarmsTopic is unspecified", () => {
+  let localProps = { ...defaultProps };
+  localProps.alarmsTopic = null;
+  const basicApp = new cdk.App();
+  const basicStack = new cdk.Stack(basicApp, "BasicStack");
+  new LogGroupWrapper(basicStack, "MyTestConstruct", localProps);
+  expectCDK(basicStack).notTo(
+    haveResource("AWS::CloudWatch::Alarm", {
+      AlarmActions: [{ Ref: "topic69831491" }]
     })
   );
 });
