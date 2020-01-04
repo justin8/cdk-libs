ba90f54c0607b94744ba4c94538c55b5c8f9e85b
Justin Dray <justin@dray.be>
2020-01-05 08:44:16 +1000

    Add static-site module

---
+++ packages/static-site/src/index.ts
@@ -0,0 +1,62 @@
+import { Construct, CfnOutput } from "@aws-cdk/core";
+import s3 = require("@aws-cdk/aws-s3");
+import s3deploy = require("@aws-cdk/aws-s3-deployment");
+import {
+  OriginAccessIdentity,
+  CloudFrontWebDistribution,
+  PriceClass
+} from "@aws-cdk/aws-cloudfront";
+import { PolicyStatement } from "@aws-cdk/aws-iam";
+import iam = require("@aws-cdk/aws-iam");
+
+export interface StaticSiteProps {
+  source?: {
+    path: string;
+  };
+  //   domainName: string;
+  //   siteSubDomain: string;
+}
+
+export class StaticSite extends Construct {
+  constructor(parent: Construct, name: string, props: StaticSiteProps) {
+    super(parent, name);
+    console.log(props); // Dummy call to allow props while they're not in use yet
+
+    const siteBucket = new s3.Bucket(this, name, {});
+    const siteOAI = new OriginAccessIdentity(this, `${name}OAI`);
+    const distribution = new CloudFrontWebDistribution(this, `${name}Dist`, {
+      priceClass: PriceClass.PRICE_CLASS_ALL,
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: siteBucket,
+            originAccessIdentity: siteOAI
+          },
+          behaviors: [{ isDefaultBehavior: true }]
+        }
+      ]
+    });
+
+    const cloudfrontBucketPolicy = new PolicyStatement();
+    cloudfrontBucketPolicy.addActions("s3:GetObject");
+    cloudfrontBucketPolicy.addResources(siteBucket.arnForObjects("*"));
+    cloudfrontBucketPolicy.effect = iam.Effect.ALLOW;
+    cloudfrontBucketPolicy.addCanonicalUserPrincipal(
+      siteOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+    );
+    siteBucket.addToResourcePolicy(cloudfrontBucketPolicy);
+
+    if (props.source) {
+      new s3deploy.BucketDeployment(this, `${name}Deployment`, {
+        sources: [s3deploy.Source.asset(props.source.path)],
+        destinationBucket: siteBucket,
+        distribution,
+        distributionPaths: ["/*"]
+      });
+    }
+
+    new CfnOutput(this, "URL", {
+      value: distribution.domainName
+    });
+  }
+}
---
+++ packages/static-site/test/cdk-static-site.test.ts
@@ -0,0 +1,155 @@
+import {
+  expect as expectCDK,
+  haveResource,
+  SynthUtils,
+  haveResourceLike
+} from "@aws-cdk/assert";
+import cdk = require("@aws-cdk/core");
+import { StaticSite } from "../lib/index";
+import { PriceClass } from "@aws-cdk/aws-cloudfront";
+import { pathToFileURL } from "url";
+import path = require("path");
+
+let defaultProps: any = {};
+
+const basicApp = new cdk.App();
+const basicStack = new cdk.Stack(basicApp, "BasicStack");
+new StaticSite(basicStack, "MyTestConstruct", defaultProps);
+
+// Update snapshot with `npm test -- -u`
+// Things to check with the snapshot:
+// * Bucket policy is unchanged
+// * CFNOutput of the domain name exists
+test("Snapshot output is unchanged", () => {
+  expect(SynthUtils.toCloudFormation(basicStack)).toMatchSnapshot();
+});
+
+test("S3 bucket created", () => {
+  expectCDK(basicStack).to(haveResource("AWS::S3::Bucket"));
+});
+
+test("OAI created", () => {
+  expectCDK(basicStack).to(
+    haveResource("AWS::CloudFront::CloudFrontOriginAccessIdentity")
+  );
+});
+
+test("Distribution created", () => {
+  expectCDK(basicStack).to(haveResource("AWS::CloudFront::Distribution"));
+});
+
+test("Distribution config is correct", () => {
+  expectCDK(basicStack).to(
+    haveResourceLike("AWS::CloudFront::Distribution", {
+      DistributionConfig: {
+        PriceClass: PriceClass.PRICE_CLASS_ALL,
+        IPV6Enabled: true,
+        HttpVersion: "http2",
+        DefaultRootObject: "index.html",
+        Origins: [
+          {
+            DomainName: {
+              "Fn::GetAtt": ["MyTestConstruct0062AECC", "RegionalDomainName"]
+            }
+          }
+        ]
+      }
+    })
+  );
+});
+
+// deployment funciton
+test("Deployment is not run by default", () => {
+  expectCDK(basicStack).notTo(haveResourceLike("Custom::CDKBucketDeployment"));
+});
+
+test("Deployment is run when source is specified", () => {
+  let localProps = {
+    ...defaultProps,
+    source: { path: path.resolve("./test/stub-deployment") }
+  };
+  const basicApp = new cdk.App();
+  const basicStack = new cdk.Stack(basicApp, "BasicStack");
+  new StaticSite(basicStack, "MyTestConstruct", localProps);
+
+  expectCDK(basicStack).to(haveResourceLike("Custom::CDKBucketDeployment"));
+});
+
+// test("Metric Filter group created", () => {
+//   expectCDK(basicStack).to(haveResource("AWS::Logs::MetricFilter"));
+// });
+
+// test("Errors Alarm created", () => {
+//   expectCDK(basicStack).to(
+//     haveResource("AWS::CloudWatch::Alarm", {
+//       MetricName: "MyTestConstructErrors"
+//     })
+//   );
+// });
+
+// test("No Logs Alarm created", () => {
+//   expectCDK(basicStack).to(
+//     haveResource("AWS::CloudWatch::Alarm", {
+//       MetricName: "IncomingLogEvents",
+//       Namespace: "AWS/Logs"
+//     })
+//   );
+// });
+
+// test("No logs alarm can be disabled", () => {
+//   let localProps = { ...defaultProps };
+//   localProps.noLogsAlarm = { enabled: false };
+//   localProps.alarmsTopic = null;
+//   const basicApp = new cdk.App();
+//   const basicStack = new cdk.Stack(basicApp, "BasicStack");
+//   new LogGroupWrapper(basicStack, "MyTestConstruct", localProps);
+//   expectCDK(basicStack).notTo(
+//     haveResource("AWS::CloudWatch::Alarm", {
+//       MetricName: "IncomingLogEvents",
+//       Namespace: "AWS/Logs"
+//     })
+//   );
+// });
+
+// test("Errors alarm can be disabled", () => {
+//   let localProps = { ...defaultProps };
+//   localProps.errorsAlarm = { enabled: false };
+//   localProps.alarmsTopic = null;
+//   const basicApp = new cdk.App();
+//   const basicStack = new cdk.Stack(basicApp, "BasicStack");
+//   new LogGroupWrapper(basicStack, "MyTestConstruct", localProps);
+//   expectCDK(basicStack).notTo(
+//     haveResource("AWS::CloudWatch::Alarm", {
+//       MetricName: "MyTestConstructErrors"
+//     })
+//   );
+// });
+
+// test("Alarms are subscribed to given topic", () => {
+//   expectCDK(basicStack).to(
+//     haveResource("AWS::CloudWatch::Alarm", {
+//       MetricName: "IncomingLogEvents",
+//       Namespace: "AWS/Logs",
+//       AlarmActions: [{ Ref: "topic69831491" }]
+//     })
+//   );
+//   expectCDK(basicStack).to(
+//     haveResource("AWS::CloudWatch::Alarm", {
+//       MetricName: "MyTestConstructErrors",
+//       AlarmActions: [{ Ref: "topic69831491" }]
+//     })
+//   );
+// });
+
+// test("No subscriptions when alarmsTopic is unspecified", () => {
+//   let localProps = { ...defaultProps };
+//   localProps.alarmsTopic = null;
+//   const basicApp = new cdk.App();
+//   const basicStack = new cdk.Stack(basicApp, "BasicStack");
+//   new LogGroupWrapper(basicStack, "MyTestConstruct", localProps);
+//   expectCDK(basicStack).notTo(
+//     haveResource("AWS::CloudWatch::Alarm", {
+//       AlarmActions: [{ Ref: "topic69831491" }]
+//     })
+//   );
+// });
